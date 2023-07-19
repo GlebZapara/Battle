8a6e31d7c028c3b7c56a191995a70647fa181cd4
GlebZapara <gleb.zapara2008@gmail.com>
2023-07-19 16:44:40 +0300

    Initial commit

--- core/src/com/badlogic/drop/Starter.java
+++ core/src/com/badlogic/drop/Starter.java
@@ -2,12 +2,9 @@ package com.badlogic.drop;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
 import java.util.Random;
 
 public class Starter extends ApplicationAdapter {
