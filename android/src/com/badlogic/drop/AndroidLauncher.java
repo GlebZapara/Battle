bca775e4834f8e91daf24d5cd79fed9fbead3c68
GlebZapara <gleb.zapara2008@gmail.com>
2023-11-13 23:45:42 +0200

    Initial commit

--- android/src/com/badlogic/drop/AndroidLauncher.java
+++ android/src/com/badlogic/drop/AndroidLauncher.java
@@ -6,12 +6,19 @@ import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
 public class AndroidLauncher extends AndroidApplication {
+	public static void main(String[] arg) {
+		AndroidLauncher androidLauncher = new AndroidLauncher();
+		androidLauncher.initializeLibGDX();
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new Starter(), config);
 	}
+
+	private void initializeLibGDX() {
+		main(new String[]{});
+	}
 }
-
-
